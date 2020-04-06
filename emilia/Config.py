ea01a115587b31d61e44474430ef7cfab0250293
സൽമാൻ <61979182+salmanpdr@users.noreply.github.com>
2020-04-06 17:02:40 +0530

    Update Config.py

--- emilia/Config.py
+++ emilia/Config.py
@@ -1,40 +1,13 @@
-  # REQUIRED
-    API_KEY = "1164745168:AAGaZZLbrseEJmtOy8ow_o7XLNksUkQWKHQ"
-    OWNER_ID = "861055237"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "tHe_GaMeR_B0Y"
-    # Some API is required for more features
-    API_OPENWEATHER = ""
-    API_ACCUWEATHER = ""
-    MAPS_API = ""
-
-    # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
-    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
-    LOAD = []
-    NO_LOAD = ['translation', 'rss', 'sed', 'weather']
-    WEBHOOK = False
-    URL = None
-
-    # OPTIONAL
-    SUDO_USERS = [1008681754 861055237]  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = [1008681754 861055237]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = [861055237 1008681754]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    DONATION_LINK = None  # EG, paypal
-    CERT_PATH = None
-    PORT = 5000
-    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
-    STRICT_GBAN = False
-    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAADBQADNwEAAjZzSxRZAc49mnFSSAI'  # banhammer marie sticker
-    ALLOW_EXCL = False  # DEPRECATED, USE BELOW INSTEAD! Allow ! commands as well as /
-    CUSTOM_CMD = False # Set to ('/', '!') or whatever to enable it, like ALLOW_EXCL but with more custom handler!
-    SPAMMERS = "" # Will not allow to interact with bot
-    TEMPORARY_DATA = None # Temporary data for backup module, use int number
-
-
-class Production(Config):
-    LOGGER = False
+from tg_bot.sample_config import Config
 
 
 class Development(Config):
-    LOGGER = True
+    OWNER_ID = 861055237  # my telegram ID
+    OWNER_USERNAME = "tHe_GaMeR_B0Y"  # my telegram username
+    API_KEY = "1164745168:AAGaZZLbrseEJmtOy8ow_o7XLNksUkQWKHQ"  # my api key, as provided by the botfather
+    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/database'  # sample db credentials
+    MESSAGE_DUMP = '--1001490304893' # some group chat that your bot is a member of
+    USE_MESSAGE_DUMP = True
+    SUDO_USERS = [861055237, 1008681754]  # List of id's for users which have sudo access to the bot.
+    LOAD = []
+    NO_LOAD = ['translation']
